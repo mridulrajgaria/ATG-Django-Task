032460d5ced08f8fee452c1c61ce18f451787d90
Mridul Rajgaria <iammridul222@gmail.com>
2025-07-17 03:33:36 +0530

    minor changes# This is a commit message template for minor changes.

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ USER_TYPE_CHOICES = (
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
-    profile_pic = models.ImageField(upload_to='profiles/')
+    profile_pic = models.ImageField(upload_to='profiles/', blank=True, null=True)
     address_line1 = models.CharField(max_length=255)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
